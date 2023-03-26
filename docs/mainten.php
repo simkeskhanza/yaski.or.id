3b4f39c3220b307f0dd9dfb2ed3027e0ed3a9d86
Yayasan SIMRS Khanza Indonesia <aski.khanzaindonesia@gmail.com>
2023-03-26 11:33:45 +0700

    Update Workshop
    Semoga Lancar

---
+++ docs/mainten.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Site Maintenance</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f7f7f7;
+        color: #333;
+        text-align: center;
+      }
+      h1 {
+        font-size: 3em;
+        margin-top: 50px;
+      }
+      p {
+        font-size: 1.5em;
+        margin-top: 30px;
+        margin-bottom: 50px;
+      }
+      img {
+        width: 500px;
+        border-radius: 50%;
+        box-shadow: 5px 5px 10px #aaa;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Site Maintenance</h1>
+    <p>We are currently performing maintenance on our website. We apologize for any inconvenience this may cause.</p>
+    <img src="https://mars.nasa.gov/system/downloadable_items/40154_mars2020_illustration_320x240.jpg" alt="Planet Mars">
+  </body>
+</html>
---
+++ docs/maintenance.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Site Maintenance</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <img src="logo.png" alt="Site Logo">
+      <h1>Site Maintenance</h1>
+      <p>We are currently performing maintenance on our website. We apologize for any inconvenience this may cause.</p>
+      <img src="construction.png" alt="Under Construction">
+    </div>
+  </body>
+</html>
